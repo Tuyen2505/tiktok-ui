cbcf9c65dd2179532c104d1db15ac62ee5ccfe64
tuyen <pmtzzp2505@gmail.com>
2024-07-20 22:45:19 +0700

    modified:   .prettierrc 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	new file:   src/components/GlobalStyles/GlobalStyles.scss 	new file:   src/components/GlobalStyles/index.js 	new file:   src/components/Layout/DefaultLayout/Sidebar/index.js 	new file:   src/components/Layout/DefaultLayout/index.js 	new file:   src/components/Layout/HeaderOnly/index.js 	new file:   src/components/Layout/components/Header/index.js 	new file:   src/components/Layout/index.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	new file:   src/pages/Following/index.js 	new file:   src/pages/Home/index.js 	new file:   src/pages/Search/index.js 	new file:   src/pages/Upload/index.js 	new file:   src/routes/index.js

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/search', component: Search, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
